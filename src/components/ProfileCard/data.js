fe9f078e378b002df59c1e2c5f715ecf4a48f2e6
Alla-Nazarenko <umnyj.start@gmail.com>
2025-01-18 12:34:46 +0100

    homework 2

---
+++ src/components/ProfileCard/data.js
@@ -0,0 +1,7 @@
+export const homeworkData = {
+    avatar: '/src/assets/foto/employee.jpg',
+    firstname: 'Alla',
+    lastname: 'Nazarenko',
+    activities: 'Development',
+    hobby: 'Gardening'
+};
