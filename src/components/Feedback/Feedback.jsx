d824fee745aec1094c2250db3833c589bfe57443
Alla-Nazarenko <umnyj.start@gmail.com>
2025-01-22 10:34:23 +0100

    homework 4 after review

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,39 +1,45 @@
 import "./styles.css";
 import Button from "../Button/Button";
+
 import { useState } from "react";
 
 function Feedback() {
   const [likes, setLikes] = useState(0);
-  const onLikeClick = () => {
-    setLikes((prevState) => prevState + 1);  };
-
-
   const [dislikes, setDislikes] = useState(0);
-  const onDislikeClick = ()=>{
-    setDislikes((prevState)=>prevState+1);
+  console.log("rerendering");
+  
+
+  const onLikeClick = () => {
+    setLikes((prevState) => prevState + 1);
   };
 
-  const onResetClick = ()=>{
-    setLikes(0)
-    setDislikes(0)
-  }
+  const onDislikeClick = () => {
+    setDislikes((prevState) => prevState + 1);
+  };
+
+  const onResetClick = () => {
+    setLikes(0);
+    setDislikes(0);
+  };
 
   return (
     <div className="feedback-container">
-      <div className="container">
-        <div className="result-container">{likes}</div>
-        <div className="hw4-button-container">
-          <Button name="Like" onClick={onLikeClick} />
+      <div className="counter-container">
+        <div className="container">
+          <div className="result-container">{likes}</div>
+          <div className="hw4-button-container">
+            <Button name="Like" onClick={onLikeClick} />
+          </div>
+        </div>
+        <div className="container">
+          <div className="result-container">{dislikes}</div>
+          <div className="hw4-button-container">
+            <Button name="Dislike" onClick={onDislikeClick} />
+          </div>
         </div>
       </div>
-      <div className="container">
-        <div className="hw4-button-container">
-          <Button name="Dislike"  onClick={onDislikeClick}/>
-        </div>
-        <div className="result-container">{dislikes}</div>
-      </div>
-      <div className="hw4-button-container">
-        <Button name="Reset Results" onClick={onResetClick}/>
+      <div className="hw4-button-container resetButton-container">
+        <Button name="RESET RESULTS" onClick={onResetClick} />
       </div>
     </div>
   );
